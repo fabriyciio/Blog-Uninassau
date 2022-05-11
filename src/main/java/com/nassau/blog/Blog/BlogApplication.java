c15eab15f1d8cc591297fd76e3a484e47347b53e
Fabricio <leitefabricio978@hotmail.com>
2022-05-10 21:42:48 -0300

    Aula 01

---
+++ src/main/java/com/nassau/blog/Blog/BlogApplication.java
@@ -0,0 +1,21 @@
+package com.nassau.blog.Blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@SpringBootApplication
+@RestController
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+		}
+		@GetMapping("/hello")
+		public String index(){
+			return "Olá, mundo";
+		}
+
+	}
